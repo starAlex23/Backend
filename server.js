75576b36b85937d86bb9ae037b8020016742ba20
starAlex23 <Alexworkings2000@gmail.com>
2025-06-30 23:38:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -452,8 +452,14 @@ async function isValidQrCode(qr) {
   return false;
 }
 
-
-
+function generateSimpleCode(length = 8) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let code = '';
+  for (let i = 0; i < length; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return code;
+}
 
 // Route zum Setzen des QR-Passworts (Admin-Zugriff erforderlich)
 app.post('/api/set-qr-passwort', authMiddleware, async (req, res) => {
