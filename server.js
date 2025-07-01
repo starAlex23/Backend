27ea22d9d6924795d2f34aa40e80dc6dfb2b18da
starAlex23 <Alexworkings2000@gmail.com>
2025-07-01 20:59:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -516,7 +516,7 @@ app.post('/api/qr/create', authMiddleware, csrfMiddleware, async (req, res) => {
       if (rowCount === 0) break; // code ist frei
     } while(true);
 
-    const gültigBis = new Date(Date.now() + 135 * 60 * 1000).toISOString(); // 15 Minuten gültig
+    const gültigBis = new Date(Date.now() + 15 * 60 * 1000).toISOString(); // 15 Minuten gültig
 
     await pool.query(
       `INSERT INTO qr_tokens (code, erstellt_von, gültig_bis) VALUES ($1, $2, $3)`,
