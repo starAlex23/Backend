a9bee0b8101cf2dd029bc7f3396a804c7110d265
starAlex23 <Alexworkings2000@gmail.com>
2025-07-11 13:54:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.use(express.static(path.join(__dirname, 'public'), {
     res.setHeader("X-Frame-Options", "SAMEORIGIN");
     res.setHeader("X-Content-Type-Options", "nosniff");
     res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
-    res.setHeader("Permissions-Policy", "camera=(), microphone=(), geolocation=()");
+    res.setHeader("Permissions-Policy", "camera=(self)");
   }
 }));
 
